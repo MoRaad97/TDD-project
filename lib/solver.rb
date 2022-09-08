3d3d0e8c55bf8ed5187eb33e3b876dfabad83c2f
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-09-08 17:53:18 +0300

    Linters Config

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,18 +1,20 @@
 class Solver
   def factorial(number)
     raise ArgumentError.new("Expected a postive value, got negative value #{number}") if number < 0
+
     result = 1
     return result if number == 0
+
     i = 1
     while i <= number
-    result = result * i
-    i += 1 
-    end 
+      result = result * i
+      i += 1
+    end
     return result
   end
 
   def reverse(word)
-       word.reverse
+    word.reverse
   end
 
   def fizzbuzz(number)
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,8 +1,8 @@
 require 'solver.rb'
 
 describe Solver do
-   solver = Solver.new
-   context 'factorial Method testing' do
+  solver = Solver.new
+  context 'factorial Method testing' do
     it 'should return 1 if the the value = 0' do
       expect(solver.factorial(0)).to eq(1)
     end
@@ -14,30 +14,30 @@ describe Solver do
     it 'should return Factorial for postive numbers' do
       expect(solver.factorial(3)).to eq(6)
     end
-   end
-   
-context 'reverse Method testing' do
-  it 'reverse' do
-    expect(solver.reverse("word")).to eq("drow")
-    expect(solver.reverse("eye")).to eq("eye")
+  end
+
+  context 'reverse Method testing' do
+    it 'reverse' do
+      expect(solver.reverse("word")).to eq("drow")
+      expect(solver.reverse("eye")).to eq("eye")
     end
-end
-
-context 'fizzbuzz Method testing' do
-  it 'should return "fizzbuzz" if the number divisible by 3 and 5' do
-    expect(solver.fizzbuzz(15)).to eq("fizzbuzz")
   end
 
-  it 'should return "fizz" if the number Only divisible by 3' do
-    expect(solver.fizzbuzz(3)).to eq("fizz")
+  context 'fizzbuzz Method testing' do
+    it 'should return "fizzbuzz" if the number divisible by 3 and 5' do
+      expect(solver.fizzbuzz(15)).to eq("fizzbuzz")
+    end
+
+    it 'should return "fizz" if the number Only divisible by 3' do
+      expect(solver.fizzbuzz(3)).to eq("fizz")
+    end
+
+    it 'should return "buzz" if the number Only divisible by 5' do
+      expect(solver.fizzbuzz(5)).to eq("buzz")
+    end
+
+    it 'should return input number as string if the number not divisible by 3 or 5' do
+      expect(solver.fizzbuzz(7)).to eq("7")
+    end
   end
-  
-  it 'should return "buzz" if the number Only divisible by 5' do
-    expect(solver.fizzbuzz(5)).to eq("buzz")
-  end
-  
-  it 'should return input number as string if the number not divisible by 3 or 5' do
-    expect(solver.fizzbuzz(7)).to eq("7")
-  end
-end
 end
